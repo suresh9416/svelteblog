386f2f4ae1fa2adacf9034229e28b91ac87f2c24
suresh <sure9416@gmail.com>
2024-05-03 18:17:34 +0800

    ganalytics

--- user/config/site.ts
+++ user/config/site.ts
@@ -58,10 +58,13 @@ export const headConfig: Site.Head = {
           // Google tag (gtag.js)
           `<script type="text/partytown" src=""></script>`,
           `<script type="text/partytown">
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-BBC849D965"></script>
+          <script>
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', '');
+          
+            gtag('config', 'G-BBC849D965');
           </script>`,
         ],
 };
